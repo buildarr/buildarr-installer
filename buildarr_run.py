ba711b0dcad583c6a21ab829d4315716cc8e129a
Callum Dickinson <callum.dickinson.nz@gmail.com>
2023-12-03 11:23:34 +1300

    Implement Buildarr installer pipeline

---
+++ buildarr_run.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2023 Callum Dickinson
+#
+# Buildarr is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Buildarr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Buildarr.
+# If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+Launch a Buildarr ad-hoc run.
+"""
+
+
+from buildarr.cli.main import main as buildarr
+
+
+def main():
+    return buildarr(["run"], standalone_mode=False)
+
+
+if __name__ == "__main__":
+    main()
