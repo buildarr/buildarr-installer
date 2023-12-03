4a44743a50f7736db4174a8f578916ce453640a3
Callum Dickinson <callum.dickinson.nz@gmail.com>
2023-12-03 14:43:14 +1300

    Add buildarr-update.bat

---
+++ buildarr-update.bat
@@ -0,0 +1,5 @@
+Python\python.exe -m pip install --upgrade buildarr ^
+                                           buildarr-sonarr ^
+                                           buildarr-radarr ^
+                                           buildarr-prowlarr[sonarr,radarr] ^
+                                           buildarr-jellyseerr[sonarr,radarr]
